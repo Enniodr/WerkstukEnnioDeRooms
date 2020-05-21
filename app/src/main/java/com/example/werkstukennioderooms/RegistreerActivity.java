bacb9faa04bbeb445bc23ca7195136cb3295ffba
Ennio De Rooms <Ennio.de.rooms@student.ehb.be>
2020-05-21 13:25:52 +0200

    In orde: Registreer pagina, string files angemaakt, Eigen checkbox en button

---
+++ app/src/main/java/com/example/werkstukennioderooms/RegistreerActivity.java
@@ -0,0 +1,14 @@
+package com.example.werkstukennioderooms;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistreerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registreer);
+    }
+}
